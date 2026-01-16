52c25bdebce8e735ccd88259a59d99cd12fb85c2
lxthxrgx <osu.bonse@gmail.com>
2026-01-16 19:03:33 +0200

    add archive model

---
+++ src/Archive/model/TransferData.cs
@@ -0,0 +1,37 @@
+﻿namespace Archive.model
+{
+    public class TransferData
+    {
+        public int Id { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public bool isActive { get; set; } = true;
+        public bool isCompleted { get; set; } = false;
+        public DateTime? CompletedAt { get; set; } = null;
+        public string ErrorMessage { get; set; } = string.Empty;
+    }
+
+    /// <summary>
+    /// this meen data(data from groups,sublease, guard by number group) transfer to archive
+    /// </summary>
+    public class TransferDataAll : TransferData
+    {
+        
+    }
+
+    public class TransferDataSublease : TransferData
+    {
+        
+    }
+    public class TransferDataGuard : TransferData
+    {
+        
+    }
+
+    /// <summary>
+    /// im not sure but maybe this is used to transfer groups of data
+    /// </summary>
+    public class TransferDataGroups : TransferData
+    {
+        
+    }
+}
